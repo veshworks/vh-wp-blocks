f6aec8cec389bbf8583d5fbee612a2a43a4e2f0f
Vinícius Hoyer <contact@vhoyer.dev>
2026-02-24 19:49:53 -0300

    🔖 Bump version to 0.1.4

--- vh-wp-blocks.php
+++ vh-wp-blocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       VH WP Blocks
  * Description:       A collection of custom Gutenberg blocks for editorial and content-rich WordPress sites.
- * Version:           0.1.3
+ * Version:           0.1.4
  * Requires at least: 6.8
  * Requires PHP:      7.4
  * Author:            vhoyer
